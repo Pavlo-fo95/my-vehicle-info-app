79cfcf3579b2252ddf7bed6a6a857ad60deee707
Alla Pavlova <Pavlo_fo95@student.itstep.org>
2024-07-18 23:43:13 +0300

    Updated UkraineMap with custom CSS

--- src/components/Car/UkraineMap.tsx
+++ src/components/Car/UkraineMap.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { SVGMap } from 'react-svg-map';
-import 'react-svg-map/lib/index.css';
 import ukraine from '@svg-maps/ukraine';
+import './UkraineMap.css'; 
 
 const UkraineMap: React.FC = () => {
-  return <SVGMap map={ukraine} />;
+  return <SVGMap map={ukraine} className="svg-map" />;
 };
 
 export default UkraineMap;
